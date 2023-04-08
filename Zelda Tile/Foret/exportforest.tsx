fa005056f0c6c8b5f037e46c703b05b03aaa1f4f
Marie <118370490+MeufKiBave@users.noreply.github.com>
2023-04-08 19:49:07 +0200

    push

---
+++ Zelda Tile/Foret/exportforest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileforest" tilewidth="64" tileheight="64" tilecount="13622" columns="139">
+ <image source="Jeutuileforest.png" width="8910" height="6300"/>
+</tileset>
---
+++ Zelda Tile/Foret/tileforest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tileforest" tilewidth="64" tileheight="64" tilecount="1426" columns="31">
+ <image source="Jeutuileforest.png" width="2000" height="3000"/>
+</tileset>
